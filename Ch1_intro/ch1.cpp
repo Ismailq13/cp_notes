de1671a49604dd02f9004d0a3855f2b056848f8d
ismailand <fakhri22003@mail.unpad.ac.id>
2025-05-10 22:41:04 +0700

    first commit learn cp

---
+++ Ch1_intro/ch1.cpp
@@ -0,0 +1,59 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    // //Input
+    // int a,b; 
+    // string x;
+    // cin >> a >> b >> x;
+    // //faster
+    // int a,b;
+    // scanf("%d %d", &a, &b);
+
+    // //Output
+    // int a = 123, b = 456;
+    // string x = "goodluckForyou";
+    // cout << a << " " << b << " " << x << "\n";
+    // // faster
+    // int a = 123, b = 456;
+    // printf("%d %d", &a, &b);
+    
+    // //Lot of Texts String 
+    // string s;
+    // getline(cin,s);
+    // cout<<s;
+
+    // //unkonwn amount of data
+    // string x;
+    // while (cin >> x){
+    //     // code
+    // }
+
+    // // read and write txt
+    // freopen("input.txt","r",stdin);
+    // freopen("output.txt","w",stdout);
+    
+    // // Integers
+    // long long x = 123456789123456789LL;
+    // int a = 123456;
+    // long long b = (long long) a*a;
+    // cout << b << "\n";
+
+    // //mod
+    // long long x = 1;
+    // int n = 4;
+    // int m = 5;
+    // for (int i=2; i<=n; i++){
+    //     x = (x*i)%m;
+    // }
+    // cout << x%m << "\n";
+    
+    // // shortcut
+    // typedef long long ll;
+    // ll a = 12345;
+    
+    return 0;
+}
